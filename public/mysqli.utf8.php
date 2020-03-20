00a01d12fe6e4949d8051b5e4679ead10cbb5ddf
Oscar García <oscar.github@linaresdigital.com>
2020-03-20 23:42:15 +0000

    Errata en juego de caracteres

--- public/mysqli.utf8.php
+++ public/mysqli.utf8.php
@@ -4,6 +4,6 @@ $mysqli = new mysqli('localhost', 'root', '', 'pruebas');
 if ($mysqli->connect_errno !== 0) {
     die('Error de conexión: '. $mysqli->connect_error);
 }
-if ($mysqli->set_charset('latin1') === false) {
+if ($mysqli->set_charset('utf8') === false) {
     die('Error cambiando juego de caracteres: '. htmlspecialchars($mysqli->error));
 }
